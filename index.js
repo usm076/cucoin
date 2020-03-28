6c5c05330839284ed11df94b8130a4854c675731
usm076 <43332999+usm076@users.noreply.github.com>
2020-03-28 10:03:19 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+var formidable=require('formidable');
+var http=require('http');
+
+http.createServer(function(req, res){
+if(req.url=='/fileupload')
+{
+   var form=new formidable.IncomingForm();
+   form.parse(req, function(error, fields, files){
+    res.write('File Uploaded');
+    res.end();
+
+   });
+
+}
+else{
+  res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+}
+
+
+}).listen(8080);
