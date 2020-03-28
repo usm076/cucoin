218fd868f187032ab6da5975b8ef6194fc072c3b
usm076 <usm076@users.noreply.github.com>
2020-03-28 10:05:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if(req.url=='/fileupload')
 }
 else{
   res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<form action="fileupload" method="post" >');
     res.write('<input type="file" name="filetoupload"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
